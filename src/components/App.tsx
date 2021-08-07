dcb7b0deec7ac986ad41aecadc2298a3f2a4f5f0
Sergey <wampwar11@gmail.com>
2021-08-07 15:41:26 +0300

    create preview

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,57 +1,64 @@
-import React, { FC, useState } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import '../styles/style.css'
 
 export const App: FC = () => {
-  const [drag, setDrag] = useState(true)
+  const [drag, setDrag] = useState<boolean>(false)
+  const [image, setImage] = useState<File | null>()
+  const [preview, setPreview] = useState<string | null>()
+
+  useEffect(() => {
+    if (image) {
+      const reader = new FileReader()
+      reader.onloadend = () => {
+        setPreview(reader.result as string)
+      }
+      reader.readAsDataURL(image)
+    } else {
+      setPreview(null)
+    }
+  }, [image])
 
   function dragStartHandler(e: any) {
     e.preventDefault()
+    e.stopPropagation()
     setDrag(true)
   }
 
   function dragLeaveHandler(e: any) {
     e.preventDefault()
+    e.stopPropagation()
     setDrag(false)
   }
 
   function onDropHandler(e: any) {
     e.preventDefault()
-    setDrag(true)
+    e.stopPropagation()
+    // const reader = new FileReader()
     const files = [...e.dataTransfer.files]
-    const reader = new FileReader()
-    reader.readAsDataURL(files[0])
-
-    console.log(reader.readAsDataURL(files[0]))
-
-    // const formData = new FormData()
-    // formData.append('file', files[0])
-    // console.log(files.length);
-    // console.log(files);
-    // console.log(FormData);
+    const file = files[0]
+    // reader.readAsDataURL(files[0])
+    console.log(file)
+    if (file) {
+      setImage(file)
+    } else {
+      setImage(null)
+    }
+    setDrag(false)
   }
 
   return (
-    <form className="app">
-      {drag ? (
-        <div
-          className="drop"
-          onDragStart={(e) => dragStartHandler(e)}
-          onDragOver={(e) => dragLeaveHandler(e)}
-          onDragLeave={(e) => dragStartHandler(e)}
-        >
-          Перетащите файл для загрузки
-        </div>
-      ) : (
-        <div
-          className="drop-area"
-          onDragStart={(e) => dragStartHandler(e)}
-          onDragOver={(e) => dragLeaveHandler(e)}
-          onDragLeave={(e) => dragStartHandler(e)}
-          onDrop={(e) => onDropHandler(e)}
-        >
-          Отпустите
+    <div
+      className={!drag ? 'wrapper' : `wrapper active`}
+      // onDragEnter={(e) => dragStartHandler(e)}
+      onDragOver={(e) => dragStartHandler(e)}
+      onDragLeave={(e) => dragLeaveHandler(e)}
+      onDrop={(e) => onDropHandler(e)}
+    >
+      {preview && (
+        <div className={'img-wrap'}>
+          <img src={preview as string} alt="123" />
         </div>
       )}
-    </form>
+    </div>
   )
 }
